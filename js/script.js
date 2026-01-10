cbc6b429b1b33f36c63d5dbf3f2551940bc3845f
flwixs <jobwarce@gmail.com>
2026-01-11 00:09:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.main-service-row').forEach(row=>{
 row.addEventListener('click',()=>{
 row.classList.toggle('active');
-const details=row.nextElementSibling;
-details.style.display = details.style.display === 'table-row' ? 'none' : 'table-row';
+const d=row.nextElementSibling;
+d.style.display=d.style.display==='table-row'?'none':'table-row';
 });
 });
