8f836408d7d875ef61b05a9592f713536a670f23
flwixs <jobwarce@gmail.com>
2026-01-11 00:19:13 +0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.main-service-row').forEach(row=>{
-row.addEventListener('click',()=>{
-row.classList.toggle('active');
-const d=row.nextElementSibling;
-d.style.display=d.style.display==='table-row'?'none':'table-row';
-});
-});
